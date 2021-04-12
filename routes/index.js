f4c9e1c3d51583bca2a5c7badc301fce6ea1b46c
Camila Atencio <camilamatencio@gmail.com>
2021-04-12 17:37:31 -0300

    Installing Axios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+
+    axios.get("https://api.mercadolibre.com/sites/MLA/search?q=celular&&limit=4")
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+    res.send("Express está funcionando");
 });
 
 module.exports = router;
