e1271934a18f976b47c1b8bc0db47465ab25e158
Camila Atencio <camilamatencio@gmail.com>
2021-04-16 16:42:45 -0300

    Implementing getSearchResults

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,24 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const axios = require('axios');
-// este es el endpoint de testAPI
-//pasar los parametros de busqueda como query, id
 
-//recibir la data de mercado libre ( ya lo hice eso)
-//tomar solo los datos que me interesan a mi
 router.get("/", function(req, res, next) {
 
 async function getSearchResults() {
   try {
     const response = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=celular&&limit=4`);
-    console.log("response");
     const eachResult = response.data.results;
+    const products = [author = {name: "Camila", lastname: "Atencio"}];
     eachResult.forEach(function(item) {
-      productInfo = {
-        author: {
-          name: "Camila",
-          lastname: "Atencio"
-        },
+      productInfo = 
+      items = [{
         id: item.id,
         title: item.title,
         price: {
@@ -28,18 +21,15 @@ async function getSearchResults() {
         picture: item.thumbnail,
         condition: item.condition,
         free_shipping: item.shipping.free_shipping
-      }
-      console.log(productInfo)
+      }];
+      products.push(productInfo)
     });
-
+    res.send(products);
   } catch (error) {
     console.error(error);
   }
 }
-
   getSearchResults();
-  
-  res.send("TEST is working properly");
 });
 
 module.exports = router;
